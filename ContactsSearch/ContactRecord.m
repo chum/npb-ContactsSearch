0c5865d326927b0c4a7f3a57ee2ecd7c1db06ffc
Olie <olie@playnaked.com>
2014-10-30 13:17:41 -0700

    sort no-name contacts to the end

--- ContactsSearch/ContactRecord.m
+++ ContactsSearch/ContactRecord.m
@@ -71,8 +71,6 @@ static NSArray *MULTIVALUE_PROPERTIES = nil;
 
 @implementation ContactRecord
 
-static const NSInteger favoriteScoreThreshhold      = 0;
-
 
 #pragma mark - Lifecycle
 
@@ -95,20 +93,20 @@ static const NSInteger favoriteScoreThreshhold      = 0;
     {
         SINGLEVALUE_PROPERTIES = @[
                // Contacts with nicknames and birthdays are likely to be more important.
-               [__DBPropertyScorePair pairWithProperty: kABPersonNicknameProperty           score: 100],
-               [__DBPropertyScorePair pairWithProperty: kABPersonBirthdayProperty           score:  50],
+               [__DBPropertyScorePair pairWithProperty: kABPersonNicknameProperty           score:  50],
+               [__DBPropertyScorePair pairWithProperty: kABPersonBirthdayProperty           score: 150],
                [__DBPropertyScorePair pairWithProperty: kABPersonFirstNameProperty          score:  10],
                [__DBPropertyScorePair pairWithProperty: kABPersonLastNameProperty           score:  10],
-               [__DBPropertyScorePair pairWithProperty: kABPersonMiddleNameProperty         score:  10],
-               [__DBPropertyScorePair pairWithProperty: kABPersonPrefixProperty             score:  10],
-               [__DBPropertyScorePair pairWithProperty: kABPersonSuffixProperty             score:  10],
-               [__DBPropertyScorePair pairWithProperty: kABPersonFirstNamePhoneticProperty  score:  10],
-               [__DBPropertyScorePair pairWithProperty: kABPersonMiddleNamePhoneticProperty score:  10],
+               [__DBPropertyScorePair pairWithProperty: kABPersonMiddleNameProperty         score:  30],
+               [__DBPropertyScorePair pairWithProperty: kABPersonPrefixProperty             score:  15],
+               [__DBPropertyScorePair pairWithProperty: kABPersonSuffixProperty             score:  20],
+               [__DBPropertyScorePair pairWithProperty: kABPersonFirstNamePhoneticProperty  score:  30],
+               [__DBPropertyScorePair pairWithProperty: kABPersonMiddleNamePhoneticProperty score:  30],
                [__DBPropertyScorePair pairWithProperty: kABPersonOrganizationProperty       score:  10],
                [__DBPropertyScorePair pairWithProperty: kABPersonJobTitleProperty           score:  10],
                [__DBPropertyScorePair pairWithProperty: kABPersonDepartmentProperty         score:  10],
-               [__DBPropertyScorePair pairWithProperty: kABPersonPrefixProperty             score:  10],
-               [__DBPropertyScorePair pairWithProperty: kABPersonNoteProperty               score:  10]
+               [__DBPropertyScorePair pairWithProperty: kABPersonPrefixProperty             score:  15],
+               [__DBPropertyScorePair pairWithProperty: kABPersonNoteProperty               score:  40]
                ];
     }
 
@@ -120,14 +118,14 @@ static const NSInteger favoriteScoreThreshhold      = 0;
               // Related names and associated dates (anniversaries) are likely to indicate
               // close relationships. Also, phone numbers and addresses rank higher than emails
               // and IM profiles.
-              [__DBPropertyScorePair pairWithProperty: kABPersonRelatedNamesProperty   score: 300],
-              [__DBPropertyScorePair pairWithProperty: kABPersonDateProperty           score: 300],
+              [__DBPropertyScorePair pairWithProperty: kABPersonRelatedNamesProperty   score: 200],
+              [__DBPropertyScorePair pairWithProperty: kABPersonDateProperty           score: 250],
               [__DBPropertyScorePair pairWithProperty: kABPersonPhoneProperty          score:  40],
-              [__DBPropertyScorePair pairWithProperty: kABPersonAddressProperty        score:  40],
-              [__DBPropertyScorePair pairWithProperty: kABPersonEmailProperty          score:  20],
-              [__DBPropertyScorePair pairWithProperty: kABPersonURLProperty            score:  10],
-              [__DBPropertyScorePair pairWithProperty: kABPersonSocialProfileProperty  score:  10],
-              [__DBPropertyScorePair pairWithProperty: kABPersonInstantMessageProperty score:  10]
+              [__DBPropertyScorePair pairWithProperty: kABPersonAddressProperty        score:  50],
+              [__DBPropertyScorePair pairWithProperty: kABPersonEmailProperty          score:  15],
+              [__DBPropertyScorePair pairWithProperty: kABPersonURLProperty            score:  25],
+              [__DBPropertyScorePair pairWithProperty: kABPersonSocialProfileProperty  score:  45],
+              [__DBPropertyScorePair pairWithProperty: kABPersonInstantMessageProperty score:  45]
               ];
     }
 }
@@ -160,7 +158,11 @@ static const NSInteger favoriteScoreThreshhold      = 0;
 
     if (lastName == nil)
     {
-        if (firstName != nil)
+        if (firstName == nil)
+        {
+            NSLog(@"%s === Warning ===  Odd contact:\n%@", __PRETTY_FUNCTION__, [self longDisplayString]);
+        }
+        else
         {
             displayString = firstName;
         }
@@ -235,6 +237,8 @@ static const NSInteger favoriteScoreThreshhold      = 0;
 {
     // Ref: http://dbader.org/blog/guessing-favorite-contacts-ios
 
+    const NSInteger favoriteScoreThreshhold      = 0;
+
     _score = 0;
 
     // Give a score penalty to contacts that belong to an organization
@@ -249,6 +253,17 @@ static const NSInteger favoriteScoreThreshhold      = 0;
     {
         CFRelease(contactKind);
     }
+
+    // if no name, we're done
+    NSString *firstName = (__bridge NSString*) (ABRecordCopyValue(abContact, kABPersonFirstNameProperty));
+    NSString *lastName  = (__bridge NSString*) (ABRecordCopyValue(abContact, kABPersonLastNameProperty));
+
+    if ((firstName == nil)
+    &&  (lastName  == nil) )
+    {
+        return;     // fast bail
+    }
+
 
     // Give score for all non-nil single-value properties
     // (e.g. first name, last name, ...).
